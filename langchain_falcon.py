d61f64a31e3269bc09c50a84b5865a61cd0f6edb
Sudarshan Koirala <snkoirala45@gmail.com>
2023-08-11 13:56:17 +0000

    update the code as the langchain factory was not used in latest version

--- langchain_falcon.py
+++ langchain_falcon.py
@@ -17,16 +17,30 @@ llm = HuggingFaceHub(huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
                      model_kwargs={"temperature":0.7, "max_new_tokens":500})
 
 
-template = """
-You are a helpful AI assistant and provide the answer for the question asked politely.
+template = """Question: {question}
 
-{question}
-"""
+Answer: Let's think step by step."""
 
-@cl.langchain_factory
-def factory():
+
+@cl.on_chat_start
+def main():
+    # Instantiate the chain for that user session
     prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
-    return llm_chain
+    # Store the chain in the user session
+    cl.user_session.set("llm_chain", llm_chain)
 
+
+@cl.on_message
+async def main(message: str):
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+
+    # Call the chain asynchronously
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+
+    # Do any post processing here
+
+    # Send the response
+    await cl.Message(content=res["text"]).send()
